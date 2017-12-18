5b5b48f0721b13bdf9b6506f1f2b2be2ec0ff84f
a1669269 <a1669269@student.adelaide.edu.au>
2017-12-18 01:03:24 +0000

    Adding five cpp files

---
+++ delta.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i=0; i<11; i++) cout << i+1 << " ";
+    cout << endl;
+    return 0;
+}
---
+++ echo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i=0; i<13; i++) cout << i+2 << " ";
+    cout << endl;
+    return 0;
+}
