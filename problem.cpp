0caf68460984e5f20b1a800c7a93cfbf0bbb97d3
a1669269 <a1669269@student.adelaide.edu.au>
2017-12-22 08:16:53 +0000

    Changes in red

--- problem.cpp
+++ problem.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main()
 {
     for (int i=3; i<8; i++) cout << i*10 << " ";
+    cout << "Making a change in red" << endl;
     cout << endl;
+    cout << "And another red change" << endl;
     return 0;
 }
