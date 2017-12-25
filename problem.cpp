d48eea7e5bd02f4d8a22af667f405d5fb67ffe0b
a1669269 <a1669269@student.adelaide.edu.au>
2017-12-25 06:40:36 +0000

    Large changes in problem

--- problem.cpp
+++ problem.cpp
@@ -8,5 +8,25 @@ int main()
     cout << endl;
     cout << "And another red change" << endl;
     cout << "Making some trouble from the black branch" << endl;
+    cout << "More and more changes" << endl;
+    for (int i=0; i<10; i++)
+    {
+        cout << "Large amount of changes" << endl;
+        cout << "More and more lines" << endl;
+        
+        cout << "Even need lines in between" << endl;
+    }
+    
+    cout << "A large set of changes" << endl;
+    
+    int n = 100;
+    for (int i=1; i<=n; i++)
+    {
+        if (n % i == 0)
+        {
+            cout << i << " is a factor of " << n << endl;
+        }
+    }
+    
     return 0;
 }
