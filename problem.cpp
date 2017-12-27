036c0ebe0b825d8615d3d455bd8146b3516a480f
a1669269 <a1669269@student.adelaide.edu.au>
2017-12-27 07:35:47 +0000

    Water changes to problem

--- problem.cpp
+++ problem.cpp
@@ -6,18 +6,18 @@ int main()
     for (int i=3; i<8; i++) cout << i*10 << " ";
     cout << "Making a change in red" << endl;
     cout << endl;
-    cout << "And another red change" << endl;
+    cout << "And another red water change" << endl;
     cout << "Making some trouble from the black branch" << endl;
     cout << "More and more changes" << endl;
     for (int i=0; i<10; i++)
     {
-        cout << "Large amount of changes" << endl;
+        cout << "Large amount of water changes" << endl;
         cout << "More and more master lines" << endl;
         
-        cout << "Even need master lines in between" << endl;
+        cout << "Even need master water lines in between" << endl;
     }
     
-    cout << "A large set of changes" << endl;
+    cout << "A large set of water changes" << endl;
     
     int n = 100;
     for (int i=1; i<=n; i++)
