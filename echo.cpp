82d39b76c6eed945a4614208027e75c1e5af5743
a1669269 <a1669269@student.adelaide.edu.au>
2017-12-27 03:39:16 +0000

    Another Maste change to E

--- echo.cpp
+++ echo.cpp
@@ -6,6 +6,7 @@ int main()
     for (int i=0; i<13; i++) cout << i+2 << " ";
     cout << endl;
     cout << "Change from master" << endl;
+    cout << "Another change from master" << endl;
     return 0;
 }
 
