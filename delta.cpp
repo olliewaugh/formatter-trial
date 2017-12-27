b409d7f7fe95e9adc15e8ab933b0bd5093e19bdf
a1669269 <a1669269@student.adelaide.edu.au>
2017-12-27 05:10:27 +0000

    Chrono 4, yellow, delta

--- delta.cpp
+++ delta.cpp
@@ -5,6 +5,7 @@ int main()
 {
     for (int i=0; i<11; i++) cout << i+1 << " ";
     cout << endl;
+    cout << "Yellow here" << endl;
     cout << "File D" << endl;
     return 0;
     // yellow comment
